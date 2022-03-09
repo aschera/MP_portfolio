8f133647d3dd38e0dcf501685fe5139158e341e1
Christina Bögh <aschera@gmail.com>
2022-03-09 21:47:09 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,157 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import "./App.css";
+import Home from './Home/Home';
+import About from './About/About';
+import Nav from './Components/Nav';
 
 
 class App extends Component {
 
-  state = {
-    title: 'Star wars movies',
-    input : '',
-    list : [],
-    loading: true,
-    movieInfo: 'No movie selected',
-    filterOptions : [ 'Sort by...', 'Episode', 'Year'],
-    url : 'https://swapi.dev/api/films/'
-  }
+  componentDidMount() {}
 
-  componentDidMount() {
-    this.api();
-  }
+  RunWhenCreated() {}
 
-  RunWhenCreated(){
-  }
-
-  filterListHandler = (e) => {
-
-    let currentFilter = this.state.filter;
-    let result = [];
-    let data = this.state.list;
-
-    /* clear list in DOM*/
-    document.getElementById('output').innerHTML = '';
-
-    /* edit filter*/
-    if(e === 'Sort by...') {
-      this.api();
-    }
-    if(e === 'Year') {
-      currentFilter = 'release_date';
-      this.setState( {
-        filter: currentFilter,
-        list: []
-      } )
-
-      result = data.sort(function (a, b) {
-        let current = a[currentFilter].slice(0, 4);
-        let next = b[currentFilter].slice(0, 4);
-        return current - next;
-      });
-    }
-
-    if(e === 'Episode') {
-      currentFilter = 'episode_id';
-      this.setState( {
-        filter: currentFilter,
-        list: []
-      } )
-
-      result = data.sort(function (a, b) {
-        return a[currentFilter] - b[currentFilter];
-      });
-    }
-
-    this.setState( {
-      list : result
-    } )
-
-  }
-
-  inputChangedHandler = (event) => {
-
-    let currentFilter = event.target.value;
-    currentFilter = currentFilter.toLowerCase();
-
-    let data = this.state.list;
-    let result = [];
-
-    /* clear list in DOM*/
-    document.getElementById('output').innerHTML = '';
-
-
-    let query = document.getElementById('filter')
-    query.innerHTML = ` ${currentFilter} <i class="fas fa-times"></i>`;
-    query.setAttribute("style",
-    "color:red; border: 1px solid blue; padding: 0.7em");
-
-
-    for (let i = 0; i < data.length; i++) {
-      var str = data[i].title;
-      str = str.toLowerCase();
-      var n = str.search(currentFilter);
-      if(n !== -1) {
-        result.push(data[i])
-      }
-      if(currentFilter === '') {
-        this.api();
-      }
-    }
-
-    this.setState( {
-      input: currentFilter,
-      list: result
-    } )
-
-  }
-
-  api() {
-    // read all entities
-    fetch(this.state.url, {
-      "method": "GET"
-    })
-    .then(response => response.json())
-    .then(response => {
-      this.setState( {
-        loading: false,
-        list : response.results
-      } )
-      this.RunWhenCreated();
-    })
-    .catch(err => { console.log(err);
-    });
-  }
-
-
-  render () {
-
+  render() {
     return (
-      <div className="App">
-
-        <h1 className="h1 title">{this.state.title}</h1>
-
-        <div className="ActiveFilter">
-          <span id="filter"></span>
-        </div>
-
-        <UserInput
-        InputName         = {this.inputChangedHandler}
-        input             = {this.state.input}
-        filterOptions     = {this.state.filterOptions}
-        filterListHandler = {this.filterListHandler} />
-
-        <UserOutput
-        loading           = {this.state.loading}
-        output            = {this.state.list}
-        movieInfo         = {this.state.movieInfo}
-        createNode        = {this.state.createNode}
-        append            = {this.state.append}
-        />
-
-      </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     );
-
   }
+
 }
 
 export default App;
