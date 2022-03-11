ce96303fb0f550cd8e693825d71e39690e199968
Christina Bögh <aschera@gmail.com>
2022-03-11 19:44:01 +0100

    updated home added nav and icons and much

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import "./App.css";
+
 import Home from './Home/Home';
 import About from './About/About';
-import Nav from './Components/Nav';
+import Awards from './Awards/Awards';
+import Contact from './Contact/Contact';
+import Education from './Education/Education';
+import Publications from './Publications/Publications';
+import Presentations from './Presentations/Presentations';
+
+import Nav from './Components/Nav/Nav';
 
 
 class App extends Component {
@@ -20,12 +28,17 @@ class App extends Component {
         <Route path="/" element={<Nav />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="education" element={<Education />} />
+          <Route path="publications" element={<Publications />} />
+          <Route path="presentations" element={<Presentations />} />
+          <Route path="awards" element={<Awards />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
 
 
       <footer className="footer-bar">
-        <Nav />
+      &copy; 2022 Miquel Pericas <Nav />
       </footer>
 
     </BrowserRouter>
